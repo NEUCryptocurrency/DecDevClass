ae66d71277d63a630c3fc4bfb10cf28f76a72ebd
Steve Flanagan <flanagan.s@husky.neu.edu>
2018-11-17 11:35:21 -0500

    More skeleton stuff, updated contract

--- Nov10/dapp.js
+++ Nov10/dapp.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', async () => {
 
+  // Default message - if a user doesn't have metamask, this will remain
   document.getElementById("dashboard").innerHTML = "<h1>DAO Dashboard is a dapp built on the Ethereum blockchain. You need an Ethereum wallet to use it - we recommend metamask, which you can install <a href = \'https://metamask.io\'>here</a></h1>";
 
   // check for metamask
@@ -9,11 +10,28 @@ window.addEventListener('load', async () => {
   } else {
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
+
+  // Try to get their account
   try {
     userAccount = web3.eth.accounts[0];
-    document.getElementById("dashboard").innerHTML = userAccount;
+    document.getElementById("dashboard").innerHTML = "";
   } catch (e) {
     console.error(e);
   }
+
+  // Web3 info
+  var daoContract = "get from remix!"
+  dao = daoContract.at("0x5f24aca0f492b897dec04a69a95a6c757107d6e8");
+
+  // Set up skeleton
+  document.getElementById("dashboard").innerHTML += "<div id = \"proposals\"><h1>Proposals</h1></div>";
+  document.getElementById("proposals").innerHTML += "<div id = \"pending-props\"><h3>Pending proposals</h3></div>";
+  document.getElementById("proposals").innerHTML += "<div id = \"passed-props\"><h3>Passed proposals</h3></div>";
+
+  // Some ideas for info we'll get
+  getPendingProposal(0);
+  getPassedProposal(0);
+  getShares(userAccount);
+  getBid(0);
 
 });
---
+++ Nov10/ourDAO.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.4.21;
+contract DAO {
+
+    mapping(address=>uint) shares;
+    struct Prop {
+        string proposal;
+        uint votesFor;
+        uint totalVotes;
+    }
+    Prop[] public proposals;
+
+    struct Bid {
+        uint price;
+        uint shares;
+        address bidder;
+        bool fulfilled;
+    }
+    Bid[] public bids;
+
+    string[] public passedProposals;
+    string public charter;
+    string public name;
+    address founder;
+    address[] board;
+    uint totalShares;
+    uint proposalFee;
+    uint sharesToVote;
+    uint votesForProp;
+
+    constructor(string _name, string _charter, uint _totalShares, uint pf, uint stv, uint vfp) public {
+        name = _name;
+        charter = _charter;
+        founder = msg.sender;
+        board.push(founder);
+        totalShares = _totalShares;
+        proposalFee = pf;
+        sharesToVote = stv;
+        votesForProp = vfp;
+        shares[founder] = totalShares;
+    }
+
+    function submitBid(uint howMany) public returns(bool success) {
+        uint pps = msg.value/howMany;
+        bids.push(Bid(pps, howMany, msg.sender, false));
+    }
+
+    function fulfillBid(uint index) public returns(bool success) {
+        require(shares[msg.sender] >= bids[index].shares);
+        require(bids[index].fulfilled == false);
+        shares[msg.sender] -= bids[index].shares;
+        shares[bids[index].bidder] += bids[index].shares;
+        msg.sender.transfer(bids[index].price * bids[index].shares);
+        bids[index].fulfilled = true;
+    }
+
+    function getShares(address owner) public returns (uint) {
+        return shares[owner];
+    }
+
+    function submitProposal(string p) public payable {
+        require(msg.value >= proposalFee);
+        proposals.push(Prop(p, 0, 0));
+    }
+
+    function implementProposal(uint index) {
+        require(proposals[index].totalVotes >= votesForProp);
+        if(proposals[index].votesFor >= proposals[index].totalVotes / 2)
+            passedProposals.push(proposals[index].proposal);
+    }
+
+    function vote(bool _vote, uint index) public returns (bool passed){
+        require(getShares(msg.sender) >= sharesToVote);
+        if(_vote)
+            proposals[index].votesFor++;
+        proposals[index].totalVotes++;
+    }
+}
